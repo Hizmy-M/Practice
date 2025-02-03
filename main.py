2e1a13c96609e98c507e5a82dfe9d4c8e2cb2776
Hizmy-M <hizmy.mohammed@gmail.com>
2025-02-03 15:37:03 +0530

    changed class

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-def main():
-    print('its working')
-    print('it was working fine')
-    print('yes yes')
+class Game:
+    def __init__(self, game_id, game_name, game_type):
+        self.game_id = game_id
+        self.game_name = game_name
+        self.game_type = game_type
 
+    def display_info(self):
+        return f'{self.game_name} is a {self.game_type}'
 
-main()
