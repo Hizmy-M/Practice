238ab6d4a8ac8167be5fc9d4c2e9cf198c3e0576
Hizmy-M <hizmy.mohammed@gmail.com>
2025-02-04 09:16:45 +0530

    edited

--- Pat-Projects/bank_account.py
+++ Pat-Projects/bank_account.py
@@ -1,12 +1,12 @@
-class Vector:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
+class Person:
+    def __init__(self, name, age):
+        self.__name = name
+        self.__age = age
+        
+    def display_info(self):
+        print(f'{self.__name}')
 
-    def __add__(self, other):
-        return Vector(self.x + other.x, self.y + other.y)
 
-v1 = Vector(10, 20)
-v2 = Vector(20, 40)
-v3 = v1 + v2
-print(f'{v3.x} {v3.y}')
+person_one = Person('asd', 'asd')
+person_one.display_info()
+
