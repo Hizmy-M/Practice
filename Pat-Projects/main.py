e63e822e56197a47c53aad801639704eab798571
Hizmy-M <hizmy.mohammed@gmail.com>
2025-02-05 16:06:01 +0530

    class created

---
+++ Pat-Projects/main.py
@@ -0,0 +1,11 @@
+class Animal:
+    def __init__(self, name):
+        self.name = name
+
+    def speak(self):
+        print(f'{self.name} makes a sound')
+
+
+class Dog(Animal):
+    def speak(self):
+        return super().speak()
