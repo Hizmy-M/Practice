f6dc52c0e0a602482cd2500eb8333a70ec9471d8
Hizmy-M <hizmy.mohammed@gmail.com>
2025-02-05 18:14:10 +0530

    class added

--- Pat-Projects/main.py
+++ Pat-Projects/main.py
@@ -1,8 +1,25 @@
-try:
-    user_input = int(input('Enter: '))
-except ValueError:
-    print('Enter only integer value')
-except TypeError:
-    print('There is an type error')
-else:
-    print(user_input)
+class Car:
+    def __init__(self, windows, doors, enginetype):
+        self.windows = windows
+        self.doors = doors
+        self.enginetype = enginetype
+
+    def drive(self):
+        print('The person drives the car')
+
+    
+car = Car(4, 5, 'Diesel')
+car.drive()
+
+
+class audi(Car):
+    def __init__(self, windows, doors, enginetype, enableai):
+        super().__init__(windows, doors, enginetype)
+        self.enableai = enableai
+
+    def selfdriving(self):
+        print('Audi supports self driving cars')
+
+new_car = audi(5, 5, 'dielsel', True)
+new_car.drive()
+new_car.selfdriving()
