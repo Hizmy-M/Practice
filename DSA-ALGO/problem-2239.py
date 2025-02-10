5b7b3cadf114b02a3f79585f466b8b71dee82555
Hizmy-M <hizmy.mohammed@gmail.com>
2025-02-10 16:19:10 +0530

    done for 224 cases

--- DSA-ALGO/problem-2239.py
+++ DSA-ALGO/problem-2239.py
@@ -2,15 +2,20 @@
 
 # Big O | Time Complexity - O(n) | Space Complexity - O(N)
 def findClosestNumber(nums):
-    lowest = abs(nums[0])
+    lowest = nums[0]
     for i in nums:
-        if abs(i) <= lowest:
-           lowest = abs(i)
+        if abs(i) < abs(lowest):
+           lowest = i
+
+    if lowest > 0:
+        return abs(lowest)
 
     return lowest
 
 
 x = findClosestNumber([-4, -2, 1, 4, 8])
 y = findClosestNumber([-100000,-100000])
+z = findClosestNumber([2,1,1,-1,100000])
 print(x)
 print(y)
+print(z)
