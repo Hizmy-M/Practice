ffc54ad31406526c25d9898e3843cccdad0a9447
Hizmy-M <hizmy.mohammed@gmail.com>
2025-02-12 22:34:51 +0530

    changed

--- Pat-Projects/bank_account.py
+++ Pat-Projects/bank_account.py
@@ -1,12 +1,24 @@
-class Person:
+class Games:
     def __init__(self, name, age):
-        self.name = name 
-        self.age = age 
+        self.name = name
+        self.age = age
 
-
-    def display(self):
-        return f"Name: {self.name} age {self.age}"
+    def display_info(self):
+        print(f"{self.name} and {self.age}") 
     
 
-person_one = Person('Test', 123)
-print(person_one.display())
+class Store:
+    def __init__(self):
+        self.games = []
+
+    def add_game(self, game):
+        self.games.append(game)
+        print(f'Game added {game.name}')
+        game.display_info()
+
+
+
+game_one = Games('GTA', 12)
+
+game_store = Store()
+game_store.add_game(game_one)
