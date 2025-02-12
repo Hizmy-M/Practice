0340e6ad0068700ec567e6c2aaeba771519377d2
Hizmy-M <hizmy.mohammed@gmail.com>
2025-02-12 22:53:09 +0530

    cleared

--- fast-api/main.py
+++ fast-api/main.py
@@ -2,10 +2,3 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get('/')
-async def root():
-    return "welcome"
-
-@app.get('/items/{item_id}')
-async def read_item(item_id):
-    return {"item_id" : {item_id}}
