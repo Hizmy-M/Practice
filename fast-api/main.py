ed73b8976249d239aae0c48db7d9177460f6954a
Hizmy-M <hizmy.mohammed@gmail.com>
2025-02-12 22:55:18 +0530

    path parameters

--- fast-api/main.py
+++ fast-api/main.py
@@ -2,3 +2,11 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+@app.get("/users/me")
+async def read_users_me():
+    return {"user_id" : "the current user"}
+
+@app.get("/users/{user_id}")
+async def read_users(user_id : int):
+    return {"user_id" : user_id}
+
